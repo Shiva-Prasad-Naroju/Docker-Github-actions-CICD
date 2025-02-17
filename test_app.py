cf0b3db1e725a3cdd4b6cfa5fd1ab899b11544a4
Shiva-Prasad-Naroju <shivanaroju26@gmail.com>
2025-02-17 17:21:30 +0530

    assert updated

--- test_app.py
+++ test_app.py
@@ -1,7 +1,6 @@
 from app import app
-
 def test_home():
-    response=app.test_client().get("/")
-
-    assert response.status_code==200
-    assert response.data=="Hello World"
+    response = app.test_client().get("/")
+    
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == "Hello World"  # ✅ Convert bytes to string
